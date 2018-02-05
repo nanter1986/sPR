d68c5272922a97592ec5fb56975f16d4e0beaa93
nanter1986 <nanter1986@gmail.com>
2018-02-05 13:41:36 +0200

    added 2 weeks values,now calc must e created

--- js/players.js
+++ js/players.js
@@ -2,217 +2,158 @@ const players=[
   {
     name:"Γκότσης Ηλίας",
     image:"img/gkotsis.jpg",
-    winsTotal:10,
-    losesTotal:2,
-    winsWeek:4,
-    losesWeek:2
+    wins:[6,4],
+    loses:[0,2]
+
   },
   {
     name:"Τουρόβνικ Ντάρια",
     image:"img/tourovnic.jpg",
-    winsTotal:11,
-    losesTotal:5,
-    winsWeek:6,
-    losesWeek:1
+    wins:[5,6],
+    loses:[4,1]
   },
   {
     name:"Δαλάκα Αικατερίνη",
     image:"img/dalaka.jpg",
-    winsTotal:12,
-    losesTotal:6,
-    winsWeek:4,
-    losesWeek:2
+    wins:[8,4],
+    loses:[4,2]
   },
   {
     name:"Αγόρου Νικόλας",
     image:"img/agorou.jpg",
-    winsTotal:10,
-    losesTotal:6,
-    winsWeek:3,
-    losesWeek:3
+    wins:[7,3],
+    loses:[3,3]
   },
   {
     name:"Ανδρονικίδου Ζωή",
     image:"img/andronikidou.jpg",
-    winsTotal:5,
-    losesTotal:3,
-    winsWeek:0,
-    losesWeek:0
+    wins:[2,3],
+    loses:[2,1]
   },
   {
     name:"Μεταξά Μελίνα",
     image:"img/metaksa.jpg",
-    winsTotal:10,
-    losesTotal:7,
-    winsWeek:3,
-    losesWeek:3
+    wins:[7,3],
+    loses:[4,3]
   },
   {
     name:"Γιακουμάτος Χάρης",
     image:"img/giakoumatos.jpg",
-    winsTotal:9,
-    losesTotal:7,
-    winsWeek:5,
-    losesWeek:3
+    wins:[4,5],
+    loses:[4,3]
   },
   {
     name:"Κρητικός Στέλιος",
     image:"img/kritikos.jpg",
-    winsTotal:8,
-    losesTotal:8,
-    winsWeek:7,
-    losesWeek:2
+    wins:[1,7],
+    loses:[6,2]
   },
   {
     name:"Τσίλης Γιάννης",
     image:"img/tsilis.jpg",
-    winsTotal:8,
-    losesTotal:9,
-    winsWeek:4,
-    losesWeek:3
+    wins:[4,4],
+    loses:[6,3]
   },
   {
     name:"Φαρμάκη Όλγα",
     image:"img/farmaki.jpg",
-    winsTotal:7,
-    losesTotal:6,
-    winsWeek:2,
-    losesWeek:4
+    wins:[5,2],
+    loses:[2,4]
   },
   {
     name:"Σαμαρινού Μαρία",
     image:"img/samarinou.jpg",
-    winsTotal:7,
-    losesTotal:7,
-    winsWeek:1,
-    losesWeek:5
+    wins:[6,1],
+    loses:[2,5]
   },
   {
     name:"Μουρούτσος Μιχάλης",
     image:"img/mouroutsos.jpg",
-    winsTotal:7,
-    losesTotal:7,
-    winsWeek:4,
-    losesWeek:3
+    wins:[3,4],
+    loses:[4,3]
   },
   {
     name:"Δικαιούλια Βιργινία",
     image:"img/dikaioulia.jpg",
-    winsTotal:7,
-    losesTotal:8,
-    winsWeek:5,
-    losesWeek:2
+    wins:[2,5],
+    loses:[6,2]
   },
   {
     name:"Παπαργυρόπουλος Νάσος",
     image:"img/papargyropoulos.jpg",
-    winsTotal:7,
-    losesTotal:9,
-    winsWeek:3,
-    losesWeek:4
-  },
-  {
-    name:"Ανδρονικίδου Ζωή",
-    image:"img/andronikidou.jpg",
-    winsTotal:5,
-    losesTotal:3,
-    winsWeek:3,
-    losesWeek:1
+    wins:[4,3],
+    loses:[5,4]
   },
   {
     name:"Πήχου Μαρίνα",
     image:"img/phxou.jpg",
-    winsTotal:5,
-    losesTotal:8,
-    winsWeek:2,
-    losesWeek:3
+    wins:[3,2],
+    loses:[5,3]
   },
   {
     name:"Θεοδωρόπουλος Θοδωρής",
     image:"img/theodoropoulos.jpg",
-    winsTotal:5,
-    losesTotal:10,
-    winsWeek:1,
-    losesWeek:6
+    wins:[4,1],
+    loses:[4,6]
   },
   {
     name:"Παρθένης Αλέξανδρος",
     image:"img/parthenis.jpg",
-    winsTotal:5,
-    losesTotal:2,
-    winsWeek:0,
-    losesWeek:0
+    wins:[5,0],
+    loses:[2,0]
   },
   {
     name:"Χατζίδου Ελένη",
     image:"img/xatzidou.jpg",
-    winsTotal:4,
-    losesTotal:4,
-    winsWeek:2,
-    losesWeek:1
+    wins:[2,2],
+    loses:[3,1]
   },
   {
     name:"Δρυμωνάκος Γιάννης",
     image:"img/drimonakos.jpg",
-    winsTotal:4,
-    losesTotal:7,
-    winsWeek:3,
-    losesWeek:3
+    wins:[1,3],
+    loses:[4,3]
   },
   {
     name:"Χαλικιά Κατερίνα",
     image:"img/xalikia.jpg",
-    winsTotal:4,
-    losesTotal:10,
-    winsWeek:0,
-    losesWeek:5
+    wins:[4,0],
+    loses:[5,5]
   },
   {
     name:"Σπυροπούλου Κωνσταντίνα",
     image:"img/spyropoulou.jpg",
-    winsTotal:3,
-    losesTotal:2,
-    winsWeek:1,
-    losesWeek:2
+    wins:[2,1],
+    loses:[0,2]
   },
   {
     name:"Θωμάς Νίκος",
     image:"img/thomas.jpg",
-    winsTotal:3,
-    losesTotal:4,
-    winsWeek:0,
-    losesWeek:2
+    wins:[3,0],
+    loses:[2,2]
   },
   {
     name:"Καπαρού Ροδάνθη",
     image:"img/kaparou.jpg",
-    winsTotal:3,
-    losesTotal:5,
-    winsWeek:1,
-    losesWeek:3
+    wins:[2,1],
+    loses:[2,3]
   },
   {
     name:"Ενέν Ξένια",
     image:"img/tourovnic.jpg",
-    winsTotal:3,
-    losesTotal:8,
-    winsWeek:3,
-    losesWeek:3
+    wins:[0,3],
+    loses:[5,3]
   },
   {
     name:"Τσεπάνης Κωνσταντίνος",
     image:"img/tsepanis.jpg",
-    winsTotal:3,
-    losesTotal:12,
-    winsWeek:2,
-    losesWeek:5
+    wins:[1,2],
+    loses:[7,5]
   },
   {
     name:"Κοκκαλίτσα Αγγελική",
     image:"img/kokalitsa.jpg",
-    winsTotal:1,
-    losesTotal:5,
-    winsWeek:1,
-    losesWeek:1
+    wins:[0,1],
+    loses:[4,1]
   }
 ]
