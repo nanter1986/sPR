f878b9d222ae226e0647524537f4c6fb26b4cfd9
nanter1986 <nanter1986@gmail.com>
2018-02-08 09:00:44 +0200

    update 080218

--- js/players.js
+++ js/players.js
@@ -1,161 +1,161 @@
-const maxGames=[15,9];
+const maxGames=[15,9,9];
 
 const players=[
   {
     name:"Γκότσης Ηλίας",
     image:"img/gkotsis.jpg",
-    wins:[6,4],
-    loses:[0,2]
+    wins:[6,4,4],
+    loses:[0,2,4]
 
   },
   {
     name:"Τουρόβνικ Ντάρια",
     image:"img/tourovnic.jpg",
-    wins:[5,6],
-    loses:[4,1]
+    wins:[5,6,3],
+    loses:[4,1,3]
   },
   {
     name:"Δαλάκα Αικατερίνη",
     image:"img/dalaka.jpg",
-    wins:[8,4],
-    loses:[4,2]
+    wins:[8,4,6],
+    loses:[4,2,2]
   },
   {
     name:"Αγόρου Νικόλας",
     image:"img/agorou.jpg",
-    wins:[7,3],
-    loses:[3,3]
+    wins:[7,3,4],
+    loses:[3,3,4]
   },
   {
     name:"Ανδρονικίδου Ζωή",
     image:"img/andronikidou.jpg",
-    wins:[2,3],
-    loses:[2,1]
+    wins:[2,3,4],
+    loses:[2,1,2]
   },
   {
     name:"Μεταξά Μελίνα",
     image:"img/metaksa.jpg",
-    wins:[7,3],
-    loses:[4,3]
+    wins:[7,3,4],
+    loses:[4,3,3]
   },
   {
     name:"Γιακουμάτος Χάρης",
     image:"img/giakoumatos.jpg",
-    wins:[4,5],
-    loses:[4,3]
+    wins:[4,5,2],
+    loses:[4,3,3]
   },
   {
     name:"Κρητικός Στέλιος",
     image:"img/kritikos.jpg",
-    wins:[1,7],
-    loses:[6,2]
+    wins:[1,7,4],
+    loses:[6,2,2]
   },
   {
     name:"Τσίλης Γιάννης",
     image:"img/tsilis.jpg",
-    wins:[4,4],
-    loses:[6,3]
+    wins:[4,4,6],
+    loses:[6,3,3]
   },
   {
     name:"Φαρμάκη Όλγα",
     image:"img/farmaki.jpg",
-    wins:[5,2],
-    loses:[2,4]
+    wins:[5,2,2],
+    loses:[2,4,2]
   },
   {
     name:"Σαμαρινού Μαρία",
     image:"img/samarinou.jpg",
-    wins:[6,1],
-    loses:[2,5]
+    wins:[6,1,2],
+    loses:[2,5,0]
   },
   {
     name:"Μουρούτσος Μιχάλης",
     image:"img/mouroutsos.jpg",
-    wins:[3,4],
-    loses:[4,3]
+    wins:[3,4,3],
+    loses:[4,3,4]
   },
   {
     name:"Δικαιούλια Βιργινία",
     image:"img/dikaioulia.jpg",
-    wins:[2,5],
-    loses:[6,2]
+    wins:[2,5,1],
+    loses:[6,2,5]
   },
   {
     name:"Παπαργυρόπουλος Νάσος",
     image:"img/papargyropoulos.jpg",
-    wins:[4,3],
-    loses:[5,4]
+    wins:[4,3,4],
+    loses:[5,4,2]
   },
   {
     name:"Πήχου Μαρίνα",
     image:"img/phxou.jpg",
-    wins:[3,2],
-    loses:[5,3]
+    wins:[3,2,1],
+    loses:[5,3,5]
   },
   {
     name:"Θεοδωρόπουλος Θοδωρής",
     image:"img/theodoropoulos.jpg",
-    wins:[4,1],
-    loses:[4,6]
+    wins:[4,1,1],
+    loses:[4,6,4]
   },
   {
     name:"Παρθένης Αλέξανδρος",
     image:"img/parthenis.jpg",
-    wins:[5,0],
-    loses:[2,0]
+    wins:[5,0,0],
+    loses:[2,0,0]
   },
   {
     name:"Χατζίδου Ελένη",
     image:"img/xatzidou.jpg",
-    wins:[2,2],
-    loses:[3,1]
+    wins:[2,2,4],
+    loses:[3,1,1]
   },
   {
     name:"Δρυμωνάκος Γιάννης",
     image:"img/drimonakos.jpg",
-    wins:[1,3],
-    loses:[4,3]
+    wins:[1,3,3],
+    loses:[4,3,3]
   },
   {
     name:"Χαλικιά Κατερίνα",
     image:"img/xalikia.jpg",
-    wins:[4,0],
-    loses:[5,5]
+    wins:[4,0,3],
+    loses:[5,5,4]
   },
   {
     name:"Σπυροπούλου Κωνσταντίνα",
     image:"img/spyropoulou.jpg",
-    wins:[2,1],
-    loses:[0,2]
+    wins:[2,1,1],
+    loses:[0,2,1]
   },
   {
     name:"Θωμάς Νίκος",
     image:"img/thomas.jpg",
-    wins:[3,0],
-    loses:[2,2]
+    wins:[3,0,1],
+    loses:[2,2,0]
   },
   {
     name:"Καπαρού Ροδάνθη",
     image:"img/kaparou.jpg",
-    wins:[2,1],
-    loses:[2,3]
+    wins:[2,1,0],
+    loses:[2,3,1]
   },
   {
     name:"Ενέν Ξένια",
     image:"img/rec.jpg",
-    wins:[0,3],
-    loses:[5,3]
+    wins:[0,3,1],
+    loses:[5,3,3]
   },
   {
     name:"Τσεπάνης Κωνσταντίνος",
     image:"img/tsepanis.jpg",
-    wins:[1,2],
-    loses:[7,5]
+    wins:[1,2,2],
+    loses:[7,5,5]
   },
   {
     name:"Κοκκαλίτσα Αγγελική",
     image:"img/kokalitsa.jpg",
-    wins:[0,1],
-    loses:[4,1]
+    wins:[0,1,0],
+    loses:[4,1,0]
   }
 ];
